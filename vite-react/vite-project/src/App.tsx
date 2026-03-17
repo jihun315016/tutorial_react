c96c37964f3069a586601f5d521a34d8da0a7404
jihun <jihun315016@gmail.com>
2026-03-17 16:32:47 +0900

    [vite-react] init

---
+++ vite-react/vite-project/src/App.tsx
@@ -0,0 +1,9 @@
+import { Component } from "react"; // 리액트 패키지에서 Component 클래스를 불러온다.
+
+class App extends Component {
+  render() {
+    return <h1>Hello, Class Component!</h1>
+  }
+}
+
+export default App;
