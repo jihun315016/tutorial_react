bd3df39402282e1024149811bc4e7fd7e5221955
jihun <jihun315016@gmail.com>
2026-03-24 16:00:54 +0900

    [vite-prject] 스타일 적용하기

--- vite-project/vite-project/src/App.tsx
+++ vite-project/vite-project/src/App.tsx
@@ -1,17 +1,26 @@
-import { useState } from 'react';
-import CountDisplay from './components/CountDisplay';
-import CountButtons from './components/CountButtons';
+import styles from 'styled-components';
+
+const Button1 = styles.button`
+  background: transparent;
+  border-radius: 3px;
+  color: #bf4f74;`
+
+// primary라는 사용자 속성을 만들어
+// 
+const Button2 = styles.button<{ $primary?: boolean }>`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #bf4f74;
+  color: ${props => props.$primary ? "black" : "#bf4f74"};`
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const reset = () => setCount(0);
-
-  return (
+  return(
     <>
-      <CountDisplay count={count} />
-      <CountButtons increment={increment} decrement={decrement} reset={reset} />
+    <Button1>Click Me1</Button1>
+    <Button2>Click Me2</Button2>
+
+    {/* 속성 이름만 주면 true */}
+    <Button2 $primary>Click Me3</Button2>
     </>
   )
 }
