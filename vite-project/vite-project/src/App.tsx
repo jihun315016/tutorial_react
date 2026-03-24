45b3b8b7a0e34a100146bb151c252e90533fa65d
jihun <jihun315016@gmail.com>
2026-03-24 17:29:03 +0900

    [vite-project] 이미지 표시

--- vite-project/vite-project/src/App.tsx
+++ vite-project/vite-project/src/App.tsx
@@ -1,26 +1,14 @@
-import styles from 'styled-components';
-
-const Button1 = styles.button`
-  background: transparent;
-  border-radius: 3px;
-  color: #bf4f74;`
-
-// primary라는 사용자 속성을 만들어
-// 
-const Button2 = styles.button<{ $primary?: boolean }>`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid #bf4f74;
-  color: ${props => props.$primary ? "black" : "#bf4f74"};`
+import img from './assets/images/heibe.jpg'
 
 export default function App() {
-  return(
+  return (
     <>
-    <Button1>Click Me1</Button1>
-    <Button2>Click Me2</Button2>
+      {/* <img src={img} alt="" /> */}
 
-    {/* 속성 이름만 주면 true */}
-    <Button2 $primary>Click Me3</Button2>
+      <div
+        style={{width:'300px', height: '320px', background: `url(${img})`}}>
+
+      </div>
     </>
-  )
+  );
 }
--- vite-project/vite-project/vite.config.ts
+++ vite-project/vite-project/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // tailwindcss 추가
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()], // 추가
 })
